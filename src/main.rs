a9bff26cedc19410177e62159de7ae7e9940a349
Avi Radinsky <avi.radinsky@gmail.com>
2021-12-05 20:30:25 -0500

    reformatting main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use rand::Rng;
 
 #[get("/<columns>/<rows>")]
 fn run(columns: u16, rows: u16) -> String {
     let mut form = String::from("{");
-    for _ in 0..columns{
+    for _ in 0..columns {
         form.push_str("\n{");
-        for _ in 0..rows{
-            if rand::thread_rng().gen_weighted_bool(2){
+        for _ in 0..rows {
+            if rand::thread_rng().gen_weighted_bool(2) {
                 form.push('1');
             } else {
                 form.push('0');
@@ -19,7 +20,7 @@ fn run(columns: u16, rows: u16) -> String {
     }
     form.pop();
     form.push_str("\n}");
-    return format!("{}",form);
+    return format!("{}", form);
 }
 #[get("/")]
 fn home() -> String {
@@ -28,5 +29,5 @@ fn home() -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![run,home])
+    rocket::build().mount("/", routes![run, home])
 }
